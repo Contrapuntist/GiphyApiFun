6ad25bd154d9ce6987efd903c88d1eeea2634bbb
Contrapuntist <contrapuntist@gmail.com>
2017-06-11 21:57:16 -0500

    some styling added and custom data attr fixed

--- javascript/giphyapp.js
+++ javascript/giphyapp.js
@@ -4,7 +4,7 @@ var gifapp = {
 
 	topics: ['puppies', 'kitties', 'baby goats'],
 	userInput: null,
-	searchlimit: null, 
+	searchlimit: 10, 
 
 	btnsRender: function() {
 
@@ -16,18 +16,22 @@ var gifapp = {
         // Loop through the array of movies, then generate buttons for each movie in the array 
 
         for (var i = 0; i < this.topics.length; i++ ) { 
-          var btn = '<button>' + [this.topics[i]] + '</button>'; 
-          $('#gifbtns').append(btn).attr('btn-data', this.topics[i]);
-          console.log(this.topics[i]);
+          var giftopic = this.topics[i];
+
+          var btn = $('<button>' + giftopic + '</button>' );
+          btn.addClass('gifbtn').attr('btn-value', giftopic); 
+          $('#gifbtns').append(btn);
+          console.log(giftopic);
         }
     },
 
-    makeImg: function(n) {
-    	
-    	var newimg = $('<img>');
-    	newimg.attr('src', response.data[0].images.downsized.url);
-    	console.log(response.data[0].images.downsized.url);
-    	$('#gifscontainer').append(newimg);
+    makeImg: function(response) {
+    	for (var i = 0; i < gifapp.searchlimit; i++) {
+	    	var newimg = $('<img>');
+	    	newimg.attr('src', response.data[i].images.downsized.url);
+	    	console.log(response.data[i].images.downsized.url);
+	    	$('#gifscontainer').append(newimg);
+	    } 
     }   
 }
 	// ie. serach http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC   
@@ -40,39 +44,37 @@ var gifapp = {
       method: 'GET'
     }).done(function(response) {
     	console.log(response); 
+    	gifapp.makeImg(response);
+    	
+
     	// $('#jsonshow').text(JSON.stringify(response)); 
 
-    	var newimg = $('<img>');
-    	newimg.attr('src', response.data[0].images.downsized.url);
-    	console.log(response.data[0].images.downsized.url);
-    	
-    	//$('.gifscontainer').html('<img src="' + response.data[0].images.downsized.url + '">' );
-    	
-    	$('.gifscontainer').append(newimg);
 
     }); 
 
 
       // This function handles events where the add movie button is clicked
-      $("#gifbtn").on("click", function(event) {
-        // event.preventDefault() prevents submit button from trying to send a form.
-        // Using a submit button instead of a regular button allows the user to hit
-        // "Enter" instead of clicking the button if desired
+      $('button').on("click", function(event) {
+      	
+      	// prevents event default action
         event.preventDefault();
 
-        // Write code to grab the text the user types into the input field
-        gifapp.userInput = ($('#movie-input').val());
-        // Write code to add the new movie into the movies array
-        gifapp.topics.push(userInput);
-        // The renderButtons function is called, rendering the list of movie buttons
+        // retrieves btn-value from button on cliick 
+        gifapp.userInput = $(this).attr('btn-value');
+        console.log(gifapp.userInput);
+
+        gifapp.topics.push(gifapp.userInput);
+        console.log(gifapp.topics);
+        
+        // call function to rerender buttons with new value
         gifapp.btnsRender();
       });
 
       // Calling the renderButtons function to display the initial list of movies
       
-      $('button').on('click', function() {
-      	gifapp.userInput = $(this).attr('btn-data');
-      	console.log(gifapp.userInput);
-      });
+      // $('button').on('click', function() {
+      // 	gifapp.userInput = $(this).attr('btn-data');
+      // 	console.log(gifapp.userInput);
+      //});
 
 });  
